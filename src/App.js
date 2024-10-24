a19c7ca58b23e54e0e0879f22bb3d1409ccb900e
POLDASUGNANESHWAR <gnaneshwarp2000@gmail.com>
2024-10-24 11:42:54 +0530

    Initial commit - Form submission challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import './App.css'
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [formData, setformData] = useState({
+  name: "",
+  message: "",
+  email: ""
+});
+
+const [submitted, setSubmitted] = useState(false);
+
+const handlesubmit = (event) => {
+  const {name,value} = event.target
+  setformData({...formData,[name]: value})
 }
 
-export default App;
+const submit = (event) => {
+  event.preventDefault()
+  setSubmitted(true)
+}
+
+return(
+  <div className="form-container">
+    <h1 >Contact US</h1>
+    <form onSubmit={submit}>
+      <div className="form-group">
+        <label htmlFor="name">Name:</label>
+        <input
+        type="text"
+        id="name"
+        name="name"
+        value={formData.name}
+        onChange={handlesubmit}
+        required
+        />
+      </div>
+      <div  className="form-group"> 
+        <label htmlFor="message">Message:</label>
+        <input 
+        type="message"
+        id="message"
+        name="message"
+        value={formData.message}
+        onChange={handlesubmit}
+        required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="email">Email:</label>
+        <input
+        type="email"
+        id="email"
+        name="email"
+        value={formData.email}
+        onChange={handlesubmit}
+        required
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+    {submitted && (
+      <p className="successMessage">Thank you! your message has been sent.</p>
+    )}
+  </div>
+)
+
+}
+export default App
